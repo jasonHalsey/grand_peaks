d1ad2d87bc957d9ca6ee819c6033dec829683b5d
Jason Halsey <jason@halsey-design.com>
2018-06-06 15:11:54 -0700

    Remove deleted partials and add phone_swap

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-// jQuery(document).ready(function() {
-//   jQuery('.x-navbar').addClass("startHead");
-// });
+jQuery(document).ready(function() {
+	 phone_swap();
+});
 
+jQuery( window ).resize(function() {
+	phone_swap();
+});
 
-
-// jQuery(function() {
-//     var header = jQuery(".startHead");
-//     jQuery(window).scroll(function() {    
-//         var scroll = jQuery(window).scrollTop();
-//     	console.log('headerFunction');
-//         if (scroll >= 500) {
-//             header.removeClass('startHead').addClass("darkHeader");
-//         } else {
-//             header.removeClass("darkHeader").addClass('startHead');
-//         }
-//     });
-// });
+function phone_swap(){
+	if (Modernizr.mq('only screen and (max-width: 769px)')) {
+		jQuery('.last_menu_contact').replaceWith('<li id="menu-item-101" class="contact-btn fa fa-lg fa-phone last_menu_contact menu-item menu-item-type-custom menu-item-object-custom menu-item-101"><a href="tel:1-562-867-5309"><span>Contact</span></a></li>');
+	}
+}
